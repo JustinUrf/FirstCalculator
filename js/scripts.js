04ed87995701f250956f9652808b410ccbd1344d
Justin Lee <lee.justin001126@gmail.com>
2023-01-09 14:11:31 -0800

    added functionthat subtracts, multiplies and divides user given integers.

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,24 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+
 //user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
-
-
-// window.alert(add(10,5));
-
-
-// // saySomething("hi");
-// // const result = add(3,5);
-// // window.alert(result);
-
-// const result = add(3, 5);
-// const outputText = "The sum is " + result + ".";
-// saySomething(outputText)
+window.alert("Your numbers added to " + add(number1, number2) +
+ ". Your numbers subtacted to " + subtract(number1, number2) + 
+ ". Your numbers multiplied to " + multiply(number1, number2) +
+ ". Your numbers divided to " + divide(number1, number2));
